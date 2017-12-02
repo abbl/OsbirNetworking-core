c69748578c1b2b20be40e259c78a641c75b4eef6
Patrick <bleowy1@gmail.com>
2017-12-02 22:46:52 +0100

    Added abstract client

--- core/src/pl/bbl/network/client/BasicClient.java
+++ core/src/pl/bbl/network/client/BasicClient.java
@@ -1,4 +1,43 @@
 package pl.bbl.network.client;
 
+
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.*;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+
 public abstract class BasicClient {
+    private String host;
+    private int port;
+
+    protected BasicClient(String host, int port){
+        this.host = host;
+        this.port = port;
+    }
+
+    public void start(){
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
+        Bootstrap bootstrap = new Bootstrap();
+
+        try {
+            bootstrap.group(workerGroup);
+            bootstrap.channel(NioSocketChannel.class);
+            bootstrap.option(ChannelOption.SO_KEEPALIVE, true);
+            bootstrap.handler(new ChannelInitializer<SocketChannel>() {
+                @Override
+                protected void initChannel(SocketChannel ch) throws Exception {
+                    addHandlersToChannel(ch.pipeline());
+                }
+            });
+        ChannelFuture channelFuture = bootstrap.connect(host, port).sync();
+        channelFuture.channel().closeFuture().sync();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            workerGroup.shutdownGracefully();
+        }
+    }
+
+    protected void addHandlersToChannel(ChannelPipeline pipeline){}
 }
