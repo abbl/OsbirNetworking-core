e915ba978042f35233a32177b605e04d862dcd20
Patrick <bleowy1@gmail.com>
2017-12-06 01:07:11 +0100

    Added a client class which you can use to test connections etc.
    this does not have any handlers by default.

---
+++ core/src/pl/bbl/network/tools/TestClient.java
@@ -0,0 +1,9 @@
+package pl.bbl.network.tools;
+
+import pl.bbl.network.client.BasicClient;
+
+public class TestClient extends BasicClient {
+    protected TestClient(String host, int port) {
+        super(host, port);
+    }
+}
