b91891817d284dab9ea69261252a6138620e3142
Patrick <bleowy1@gmail.com>
2017-12-27 17:52:42 +0100

    PacketReceiver is now able to send packets if AbstractClient was passed in constructor.

--- core/src/pl/bbl/network/server/handlers/PacketReceiver.java
+++ core/src/pl/bbl/network/server/handlers/PacketReceiver.java
@@ -1,16 +1,31 @@
 package pl.bbl.network.server.handlers;
 
+import pl.bbl.network.client.AbstractClient;
 import pl.bbl.network.packet.Packet;
+import pl.bbl.network.tools.NetworkLogger;
 
 public abstract class PacketReceiver {
     private String receiverType;
+    private AbstractClient abstractClient;
 
     protected PacketReceiver(String receiverType){
         this.receiverType = receiverType;
+    }
+
+    protected PacketReceiver(String receiverType, AbstractClient abstractClient){
+        this(receiverType);
+        this.abstractClient = abstractClient;
     }
 
     public boolean isReceiverTypeEqual(String otherReceiverType){
         return receiverType.equals(otherReceiverType);
     }
     public abstract boolean receive(Packet packet);
+
+    protected void sendPacket(Packet packet){
+        if(abstractClient != null)
+            abstractClient.write(packet);
+        else
+            NetworkLogger.log("Stopped a attempt to send a packet in PacketReceiver because AbstractClient is equal null.");
+    }
 }
---
+++ core/src/pl/bbl/network/tools/NetworkLogger.java
@@ -0,0 +1,7 @@
+package pl.bbl.network.tools;
+
+public class NetworkLogger {
+    public static void log(String message){
+        System.out.println("[NETWORK_FRAMEWORK] " + message);
+    }
+}
