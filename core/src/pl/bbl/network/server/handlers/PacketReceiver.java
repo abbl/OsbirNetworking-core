22ba28a833234de8f74d763cb80f867835c53bac
Patrick <bleowy1@gmail.com>
2017-12-28 21:10:15 +0100

    Small change to PacketReceiver.

--- core/src/pl/bbl/network/server/handlers/PacketReceiver.java
+++ core/src/pl/bbl/network/server/handlers/PacketReceiver.java
@@ -2,11 +2,13 @@ package pl.bbl.network.server.handlers;
 
 import pl.bbl.network.client.AbstractClient;
 import pl.bbl.network.packet.Packet;
+import pl.bbl.network.server.connection.AbstractUser;
 import pl.bbl.network.tools.NetworkLogger;
 
 public abstract class PacketReceiver {
     protected AbstractClient abstractClient;
-    private String receiverType;
+    protected AbstractUser abstractUser;
+    protected String receiverType;
 
     protected PacketReceiver(String receiverType){
         this.receiverType = receiverType;
@@ -17,15 +19,23 @@ public abstract class PacketReceiver {
         this.abstractClient = abstractClient;
     }
 
+    protected PacketReceiver(String receiverType, AbstractUser abstractUser){
+        this(receiverType);
+        this.abstractUser = abstractUser;
+    }
+
     public boolean isReceiverTypeEqual(String otherReceiverType){
         return receiverType.equals(otherReceiverType);
     }
-    public abstract boolean receive(Packet packet);
 
     protected void sendPacket(Packet packet){
         if(abstractClient != null)
             abstractClient.write(packet);
+        else if(abstractUser != null)
+            abstractUser.sendPacket(packet);
         else
-            NetworkLogger.log("Stopped a attempt to send a packet in PacketReceiver because AbstractClient is equal null.");
+            NetworkLogger.log("Stopped a attempt to send a packet in PacketReceiver because AbstractClient or AbstractUser is equal null.");
     }
+
+    public abstract boolean receive(Packet packet);
 }
